10c886b65bce19baa116ee1cdabd97cebdf4987d
codewithdarkwa <jnrdrkw1@gmail.com>
2023-09-27 19:47:31 +0000

    explore and thread post screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:thread_clone_flutter/screens/favorite_screen.dart';
-import 'package:thread_clone_flutter/screens/feed.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:thread_clone_flutter/screens/post_screen.dart';
 import 'package:thread_clone_flutter/screens/profile_screen.dart';
 
+import 'favorite_screen.dart';
+import 'feed.dart';
 import 'search.dart';
 
 class Home extends StatefulWidget {
@@ -16,17 +17,37 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   int selectedIndex = 0;
 
-  List<Widget> pages = [
-    const FeedScreen(),
-    const SearchScreen(),
-    const PostScreen(),
-    const FavoriteScreen(),
-    const ProfileScreen(),
-  ];
+  List<Widget> pages = [];
+  PanelController panelController = PanelController();
+
+  @override
+  void initState() {
+    pages = [
+      const FeedScreen(),
+      const SearchScreen(),
+      PostScreen(panelController: panelController),
+      const FavoriteScreen(),
+      const ProfileScreen(),
+    ];
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: pages[selectedIndex],
+      body: SlidingUpPanel(
+        controller: panelController,
+        minHeight: 0,
+        maxHeight: MediaQuery.of(context).size.height * 0.8,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(25),
+          topRight: Radius.circular(25),
+        ),
+        panelBuilder: (ScrollController sc) {
+          return PostScreen(panelController: panelController);
+        },
+        body: pages[selectedIndex],
+      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: selectedIndex,
         selectedItemColor: Colors.black,
@@ -34,9 +55,16 @@ class _HomeState extends State<Home> {
         showSelectedLabels: false,
         showUnselectedLabels: false,
         onTap: (index) {
-          setState(() {
-            selectedIndex = index;
-          });
+          if (index == 2) {
+            panelController.isPanelOpen
+                ? panelController.close()
+                : panelController.open();
+          } else {
+            panelController.close();
+            setState(() {
+              selectedIndex = index;
+            });
+          }
         },
         type: BottomNavigationBarType.fixed,
         items: const [
--- lib/screens/post_screen.dart
+++ lib/screens/post_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/src/panel.dart';
 
 class PostScreen extends StatefulWidget {
-  const PostScreen({super.key});
+  const PostScreen({
+    super.key,
+    required this.panelController,
+  });
 
+  final PanelController panelController;
   @override
   State<PostScreen> createState() => _PostScreenState();
 }
@@ -10,9 +15,75 @@ class PostScreen extends StatefulWidget {
 class _PostScreenState extends State<PostScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("PostScreen"),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    widget.panelController.close();
+                  },
+                  child: const Text(
+                    'Cancel',
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const Text(
+                  'New thread',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Post',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Divider(thickness: 1),
+          Padding(
+            padding: const EdgeInsets.only(top: 12.0),
+            child: Row(
+              children: [
+                const CircleAvatar(
+                  foregroundImage: AssetImage('assets/profile.png'),
+                  radius: 25,
+                ),
+                const SizedBox(
+                  width: 14,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'codewithdarkwa',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: 'Start a thread...',
+                          hintStyle: TextStyle(fontSize: 14),
+                          border: InputBorder.none,
+                        ),
+                        maxLines: null,
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
