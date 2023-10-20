e1046a57fe0f376240074477adfbf90b24e547d2
codewithdarkwa <jnrdrkw1@gmail.com>
2023-10-20 12:36:34 +0000

    update

--- lib/screens/edit_profile.dart
+++ lib/screens/edit_profile.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:sliding_up_panel/src/panel.dart';
 import 'package:thread_clone_flutter/model/user.dart';
 
@@ -13,7 +19,63 @@ class EditProfile extends StatefulWidget {
 }
 
 class _EditProfileState extends State<EditProfile> {
+  final bioController = TextEditingController();
+  final linkController = TextEditingController();
+  final currentUser = FirebaseAuth.instance.currentUser;
   bool isChecked = false;
+
+  String profileImageUrl = "";
+
+  Future<void> updateUserProfile() async {
+    try {
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(currentUser!.uid)
+          .set({
+        'bio': bioController.text,
+        'link': linkController.text,
+        'profileImageUrl': profileImageUrl
+      }, SetOptions(merge: true));
+
+      widget.panelController.close();
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  Future<void> uploadImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      final file = File(pickedFile.path);
+      final storageRef = FirebaseStorage.instance
+          .ref()
+          .child('profile_images/${currentUser!.uid}.jpg');
+      try {
+        final upload = storageRef.putFile(file);
+        final snaphot = await upload.whenComplete(() {});
+        final downloadUrl = await snaphot.ref.getDownloadURL();
+
+        setState(() {
+          profileImageUrl = downloadUrl;
+        });
+      } catch (e) {
+        debugPrint(e.toString());
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    profileImageUrl = widget.user?.profileImageUrl ??
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRz8cLf8-P2P8GZ0-KiQ-OXpZQ4bebpa3K3Dw&usqp=CAU";
+
+    bioController.text = widget.user?.bio ?? "";
+    linkController.text = widget.user?.link ?? "";
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -40,7 +102,7 @@ class _EditProfileState extends State<EditProfile> {
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
                 ),
                 TextButton(
-                  onPressed: () {},
+                  onPressed: updateUserProfile,
                   child: const Text(
                     'Done',
                     style: TextStyle(fontWeight: FontWeight.bold),
@@ -65,11 +127,12 @@ class _EditProfileState extends State<EditProfile> {
                   children: [
                     ListTile(
                       title: const Text('Name'),
-                      subtitle: const Text('username'),
+                      subtitle: Text(
+                          '${widget.user?.name} (@${widget.user?.username})'),
                       trailing: InkWell(
-                        onTap: () {},
-                        child: const CircleAvatar(
-                          backgroundImage: AssetImage('assets/profile.png'),
+                        onTap: uploadImage,
+                        child: CircleAvatar(
+                          backgroundImage: NetworkImage(profileImageUrl),
                           radius: 20,
                         ),
                       ),
@@ -78,6 +141,7 @@ class _EditProfileState extends State<EditProfile> {
                     ListTile(
                       title: const Text('Bio'),
                       subtitle: TextFormField(
+                        controller: bioController,
                         decoration: const InputDecoration(
                           border: InputBorder.none,
                           hintText: 'Bio needs to be here...',
@@ -89,6 +153,7 @@ class _EditProfileState extends State<EditProfile> {
                     ListTile(
                       title: const Text('Link'),
                       subtitle: TextFormField(
+                        controller: linkController,
                         decoration: const InputDecoration(
                           border: InputBorder.none,
                           hintText: 'youtube.com/@codewithdarkwa',
